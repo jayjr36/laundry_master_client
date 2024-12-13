a8d2af7e92cb2e5f79cae6691343dc5df3ee105e
john <johnjr092@gmail.com>
2024-12-13 12:47:53 +0300

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:laundry_master/firebase_options.dart';
-import 'package:laundry_master/login.dart';
 import 'package:laundry_master/splash.dart';
 
 void main() async {
